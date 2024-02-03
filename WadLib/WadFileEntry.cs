e02e209a77dbd91524fb2c27eeb8d3d8f2a7111b
morgana <89588301+morgana-x@users.noreply.github.com>
2024-02-03 18:50:19 +1100

    Add files via upload

--- WadLib/WadFileEntry.cs
+++ WadLib/WadFileEntry.cs
@@ -12,8 +12,12 @@ namespace WadLib
         public string FileName { get; set; } = "NOFILENAME";
         public long FileSize { get; set; } = 0; // int 64, 8 bytes
         public long FileOffset { get; set; } = 0; // int 64, 8 bytes The offset is from the beginning of the file data section in the parent WAD
+
+        public long EntryOffset { get; set; } = 0; // location of where this file entry is at so it can be edited
         public void ReadData(Stream stream)
         {
+
+            EntryOffset = stream.Position;
             byte[] temp32intBuffer = new byte[4];
             byte[] temp64intBuffer = new byte[8];
 
@@ -28,6 +32,14 @@ namespace WadLib
 
             stream.Read(temp64intBuffer);
             FileOffset = BitConverter.ToInt64(temp64intBuffer);
+        }
+        public void WriteData(Stream stream)
+        {
+            stream.Position = EntryOffset;
+            stream.Write(BitConverter.GetBytes(FileName.Length));
+            stream.Write(Encoding.Default.GetBytes(FileName));
+            stream.Write(BitConverter.GetBytes(FileSize));
+            stream.Write(BitConverter.GetBytes(FileOffset));
         }
     }
 }
